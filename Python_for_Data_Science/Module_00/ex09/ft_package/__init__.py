44da3461b03785d85183d40657233babc6b1c40e
Vitaliy Shavnin <crowf4i@gmail.com>
2023-09-30 22:49:43 +0200

    update

---
+++ Python_for_Data_Science/Module_00/ex09/ft_package/__init__.py
@@ -0,0 +1,2 @@
+"""This is the ft_package module."""
+from .my_module import count_in_list
--- Python_for_Data_Science/Module_00/ex09/setup.py
+++ Python_for_Data_Science/Module_00/ex09/setup.py
@@ -3,19 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='ft_package',
     version='0.0.1',
-    description='list element counter',
+    description='List countere',
     author='cdarrell',
     author_email='cdarrell@42.fr',
     url='https://github.com/CroWcbr/Piscine_42_Python_-_ML/tree/main/Python_for_Data_Science/Module_00/ex09/ft_package',
     packages=find_packages(),
-    classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-		'Programming Language :: Python :: 3.9',
-		'Programming Language :: Python :: 3.10',
-    ],
-    install_requires=[],
+    license='MIT',
 )
