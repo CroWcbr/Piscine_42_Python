3a7f8af915b0f2c5eb3279bd8fe13f05d2ce2939
Vitaliy Shavnin <crowf4i@gmail.com>
2023-09-30 19:03:09 +0200

    update

--- Python_for_Data_Science/Module_00/ex09/setup.py
+++ Python_for_Data_Science/Module_00/ex09/setup.py
@@ -6,7 +6,7 @@ setup(
     description='list element counter',
     author='cdarrell',
     author_email='cdarrell@42.fr',
-    url='https://github.com/CroWcbr/Piscine_42_Python_-_ML/tree/main/Python%20for%20Data%20Science/Module_00/ex09/ft_package',
+    url='https://github.com/CroWcbr/Piscine_42_Python_-_ML/tree/main/Python_for_Data_Science/Module_00/ex09/ft_package',
     packages=find_packages(),
     classifiers=[
         'License :: OSI Approved :: MIT License',
